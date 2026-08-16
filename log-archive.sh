5a3f94924351b0c3103ca506ada75c3338ca287c
João <jo.gabrielnevess@gmail.com>
2026-08-16 10:51:05 -0300

    Aceitando lista de diretorios para compactação de logs

--- log-archive.sh
+++ log-archive.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-log_archive() {
+validate_dir() {
     local dir="${1:-}"
-    local date_time
-    local save_dir="$HOME/archives"
-
-    date_time=$(date +%Y%m%d_%H%M%S)
-
     if [[ -z "$dir" ]]; then
         echo "Uso: $0 <arquivo-ou-diretório>"
         return 1
@@ -16,15 +11,31 @@ log_archive() {
         echo "Erro: caminho não encontrado: $dir"
         return 1
     fi
+}
 
-    echo "Compactando logs: $dir"
+log_archive() {
+    local date_time
+    local save_dir="$HOME/archives"
+
+    date_time=$(date +%Y%m%d_%H%M%S)
+
+    if [[ $# -eq 0 ]]; then
+        echo "Nenhum arquivo recebido"
+        return 1
+    fi
+
+    for dir in $@; do 
+        if ! validate_dir "$dir" ; then return 1; fi
+    done
+
+    echo "Compactando logs"
     
     if ! mkdir -p "$save_dir"; then
         echo "Erro ao criar diretório: $save_dir"
         return 1
     fi
     
-    if tar -czvf $save_dir/logs_archive_${date_time}.tar.gz "$dir"; then
+    if tar -czvf $save_dir/logs_archive_${date_time}.tar.gz "$@"; then
         echo "Arquivo de log compactado com sucesso e salvo em $save_dir"
     else
         echo "Erro ao compactar arquivo!"
@@ -32,4 +43,4 @@ log_archive() {
     fi
 }
 
-log_archive "$1"
+log_archive "$@"
